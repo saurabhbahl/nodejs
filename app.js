d2bafd8ea0d0ab1b9643997b64e5d399e959fa88
Pankaj <pankaj92999@gmail.com>
2017-05-11 17:39:35 +0530

    Buffer Practice
    This is buffer practice file.

---
+++ app.js
@@ -0,0 +1,15 @@
+//var buff = new Buffer("Start It");
+
+//console.log("Buffer Writtern is: "+ buff.toJSON());
+
+// Concatenate Buffers
+/*
+var b1 = new Buffer("Hello buffer1");
+var b2 = new Buffer("\nHello buffer2"); 
+
+var b3 = Buffer.concat([b1, b2]);
+
+console.log("Buffer Cncatenate : "+ b3);
+
+*/
+
